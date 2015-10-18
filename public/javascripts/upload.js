fbbf099c5e025b0f50d12bc4461347093dc6b40e
khp <kanghee.p@gmail.com>
2015-10-18 07:41:57 -0700

    some stuff agwaepotrhpue

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,35 +1,65 @@
+// $(':button').click(function(){
+//     var formData = new FormData($('form')[0]);
+//     console.log(formData);
+//     console.log("here");
+//     $.ajax({
+//         url: '/search',  //Server script to process data
+//         type: 'POST',
+//         xhr: function() {  // Custom XMLHttpRequest
+//             var myXhr = $.ajaxSettings.xhr();
+//             if(myXhr.upload){ // Check if upload property exists
+//                 myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
+//             }
+//             return myXhr;
+//         },
+//         //Ajax events
+//         // beforeSend: beforeSendHandler,
+//         success: completeHandler,
+//         // error: errorHandler,
+//         // Form data
+//         data: formData,
+//         //Options to tell jQuery not to process data or worry about content-type.
+//         cache: false,
+//         contentType: false,
+//         processData: false
+//     });
+// });
+
+// function progressHandlingFunction(e){
+//     if(e.lengthComputable){
+//         $('progress').attr({value:e.loaded,max:e.total});
+//     }
+// }
+
+// function completeHandler() {
+
+// }
+
+var imgElem = document.getElementById('img');
+$('#urlText').keyup(function(){
+   $('#img').attr('src',$('#urlText').val());
+});
+   
 $(':button').click(function(){
-    var formData = new FormData($('form')[0]);
-    console.log("here");
-    $.ajax({
-        url: '/search',  //Server script to process data
-        type: 'POST',
-        xhr: function() {  // Custom XMLHttpRequest
-            var myXhr = $.ajaxSettings.xhr();
-            if(myXhr.upload){ // Check if upload property exists
-                myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
-            }
-            return myXhr;
-        },
-        //Ajax events
-        // beforeSend: beforeSendHandler,
-        success: completeHandler,
-        // error: errorHandler,
-        // Form data
-        // data: formData,
-        //Options to tell jQuery not to process data or worry about content-type.
-        cache: false,
-        contentType: false,
-        processData: false
-    });
+var imgData = JSON.stringify(getBase64Image(imgElem));
+  $.ajax({
+  url: '/search',
+  dataType: 'json',
+  data: imgData,
+  type: 'POST',
+  success: function(data) {
+    console.log(data);
+    }
+  });
 });
 
-function progressHandlingFunction(e){
-    if(e.lengthComputable){
-        $('progress').attr({value:e.loaded,max:e.total});
-    }
-}
-
-function completeHandler() {
-
+function getBase64Image(imgElem) {
+// imgElem must be on the same server otherwise a cross-origin error will be thrown "SECURITY_ERR: DOM Exception 18"
+    var canvas = document.createElement("canvas");
+    canvas.width = imgElem.clientWidth;
+    canvas.height = imgElem.clientHeight;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(imgElem, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var clarifai;
+
+var baseUrl = 'https://api-alpha.clarifai.com/v1/';
+var collectionId = 'default';
+var accessToken = 'zRigm7MryebDi9RUlpknO8nCmjNBvW';
+var nameSpace = 'default';
+var collectionId = 'default';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,7 +16,48 @@ router.get('/index2', function(req, res, next) {
   res.render('index2');
 });
 router.post('/search', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	console.log(req.body);
+  getTagging(req);
+  // clarifai.getTagging(req);
+  res.render('index');
 });
 
 module.exports = router;
+
+function init(){
+    clarifai = new Clarifai(
+        {
+            'clientId': 'HM0gxfG9Cu4UyMS_c2HrieEUasbfF-VMr0WajYt-',
+            'clientSecret': 'bzF3jssSAxDodvkEWK0VH3JMVE-q-Z59XCBzsyOM'
+        }
+    );
+}
+
+getTagging = function(obj) {
+    if (obj != null) {
+        console.log(obj.body);
+        $.ajax(
+            {
+                'type': 'POST',
+                'url': baseUrl + 'v1/tag/',
+                'data': obj.body,
+                'processData': false,
+                'contentType': 'application/json; charset=utf-8',
+                'headers': {
+                    'Authorization': 'Bearer ' + accessToken
+                }
+            } 
+        ).then(
+        function(json){
+            if(json.status.status === "OK"){
+              console.log(json);
+            }
+            if(json.status.status === 'ERROR'){
+            }
+        },
+        function(e){
+          console.log('error');
+        }
+    );
+    }
+}
